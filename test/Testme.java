d51d705e570f9bb52e15229e0a53fbd38c42f3de
Ricardo Viana <ricardo@onlinebunker.com>
2021-02-23 15:30:36 -0600

    httpd added

--- test/Testme.java
+++ test/Testme.java
@@ -1,6 +1,6 @@
 public class Testme {
   public final static void main(String[] args) {
     System.out.println("Exception from /app/Testme.class - jenkinstest");
-    throw new RuntimeException("TEXT OF THE EXCEPTION");
+    //throw new RuntimeException("TEXT OF THE EXCEPTION");
   }
 }
