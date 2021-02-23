3ff0ad94838a83ae7bd1365b84bb498332e4b76e
Ricardo Viana <ricardo@onlinebunker.com>
2021-02-23 13:17:18 -0600

    Added

--- test/Testme.java
+++ test/Testme.java
@@ -1,5 +1,6 @@
 public class Testme {
   public final static void main(String[] args) {
-    System.out.println("Hello from /app/Testme.class - jenkinstest");
+    System.out.println("Exception from /app/Testme.class - jenkinstest");
+    throw new RuntimeException("TEXT OF THE EXCEPTION");
   }
 }
